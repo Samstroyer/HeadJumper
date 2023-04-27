cf7f7f789ca81cfa7ef392e45204de2b8af797b8
Samuel Palmer <palmer.samuel@elev.ga.ntig.se>
2023-04-27 21:12:07 +0200

    Made larger particles to easier see them

--- HeadJumper/Particle.cs
+++ HeadJumper/Particle.cs
@@ -6,20 +6,26 @@ internal class Particle
     private static Random r = new();
     static Vector2 bounds = new(20, 20);
     internal Vector2 position;
-    private Vector2 speed;
 
     internal Particle()
     {
         position = new(r.Next((int)-bounds.X, (int)bounds.X), r.Next(0, (int)bounds.Y));
-        speed = new((float)r.Next(50, 150) / 100, (float)-r.Next(50, 150) / 100);
     }
 
     internal void UpdateAndRender(Color c)
     {
-        Raylib.DrawRectangle((int)position.X + (int)Player.Position.X + (int)bounds.X, (int)position.Y + (int)Player.Position.Y + (int)bounds.Y, 2, 2, c);
+        int xPos = (int)position.X + (int)Player.Position.X + (int)bounds.X;
+        int yPos = (int)position.Y + (int)Player.Position.Y + (int)bounds.Y;
+        Raylib.DrawRectangle(xPos, yPos, 4, 4, c);
 
-        position += speed;
-        if (position.X > bounds.X) position.X = (int)-bounds.X;
-        if (position.Y < -bounds.Y) position.Y = bounds.Y;
+        position += new Vector2(r.Next(-1, 2), -r.Next(1, 2));
+        if (position.X > bounds.X
+            || position.X < -bounds.X
+            || position.Y < -bounds.Y) Respawn();
+    }
+
+    private void Respawn()
+    {
+        position = new(r.Next((int)-bounds.X, (int)bounds.X), bounds.Y);
     }
 }
